a7f9ddf966c0044074ce672f17d85a2b27cdbe7a
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2020-06-21 14:53:36 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,6 @@ command=$3
 # set the owner to the 'build' user, so it can access package files
 sudo chown -R build /github/workspace /github/home
 
-install_deps() {
-    # install make and regular package dependencies
-    grep -E 'depends|makedepends' PKGBUILD | \
-        sed -e 's/.*depends=//' -e 's/ /\n/g' | \
-        tr -d "'" | tr -d "(" | tr -d ")" | \
-        xargs yay -S --noconfirm
-}
-
 case $target in
     pkgbuild)
         namcap PKGBUILD
@@ -23,15 +15,10 @@ case $target in
         PKGEXT='.pkg.tar' makepkg --syncdeps --noconfirm
         namcap "${pkgname}"-*.pkg.tar
         ;;
-    run)
-        install_deps
-        PKGEXT='.pkg.tar' makepkg --syncdeps --noconfirm --install
-        eval "$command"
-        ;;
     srcinfo)
         makepkg --printsrcinfo | diff .SRCINFO - || \
             { echo ".SRCINFO is out of sync. Please run 'makepkg --printsrcinfo' and commit the changes."; false; }
         ;;
     *)
-      echo "Target should be one of 'pkgbuild', 'srcinfo', 'run'" ;;
+      echo "Target should be one of 'pkgbuild', 'srcinfo'" ;;
 esac
